57357490c010721ca5259f22e037603410d934b8
kylefarmer85 <kylefarmer85@gmail.com>
2020-12-01 22:58:09 -0600

    Done.

--- src/reducers/manageBand.js
+++ src/reducers/manageBand.js
@@ -1,11 +1,16 @@
+import { v4 as uuidv4 } from 'uuid';
+
 export default function manageBand(state = {
   bands: []
 }, action) {
   switch (action.type) {
     case 'ADD_BAND':
+      const band = { id: uuidv4(), name: action.name }
+      return { ...state, bands: [...state.bands, band] }
 
-      return { ...state, bands: [...state.bands, action.name] }
-
+    case 'DELETE_BAND':
+      const bands = state.bands.filter(band => band.id !== action.id);
+      return { bands };
     default:
       return state;
   }
